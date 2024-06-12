ffec63676dfc0a28ba4c36a244c1cfbe2272f8f2
Aaron <aarontaylorg33k@gmail.com>
2024-06-11 20:25:30 -0400

    Better

--- src/components/home/halftone.tsx
+++ src/components/home/halftone.tsx
@@ -1,10 +1,11 @@
 import { cubicBezier, motion, useTransform } from "framer-motion";
 export default function HyperPinkBreakpoints({ scrollYProgress }: { scrollYProgress?: any }){
     const cB = cubicBezier(0.17, 0.67, 0.83, 0.67);
-    const y = useTransform(scrollYProgress, [0,1],['-50vh','100vh'], {ease: cB})
+    const y = useTransform(scrollYProgress, [0,1],['0vh','-50vh'], {ease: cB})
+    const height = useTransform(scrollYProgress, [0,1],['100%','200%'], {ease: cB})
     return (
-        <motion.div className="w-full h-full z-[10s] absolute pointer-events-none card overflow-hidden">
-            <motion.div className="halftone  absolute top-0 left-0 right-0 bottom-0" style={{y}}></motion.div>
+        <motion.div className="w-full h-[full] z-[10s] absolute pointer-events-none card overflow-hidden">
+            <motion.div className="halftone  w-full h-[400vh]" style={{y}}></motion.div>
         </motion.div>
     );
 };
