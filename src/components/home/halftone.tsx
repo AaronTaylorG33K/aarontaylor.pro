42442d8ecacb1a54100f00a3c0aa07aafed916e8
Aaron <aarontaylorg33k@gmail.com>
2024-06-11 20:51:11 -0400

    Tighten the gap

--- src/components/home/halftone.tsx
+++ src/components/home/halftone.tsx
@@ -1,11 +1,12 @@
 import { cubicBezier, motion, useTransform } from "framer-motion";
 export default function HyperPinkBreakpoints({ scrollYProgress }: { scrollYProgress?: any }){
     const cB = cubicBezier(0.17, 0.67, 0.83, 0.67);
-    const y = useTransform(scrollYProgress, [0,1],['0vh','-50vh'], {ease: cB})
+    const y = useTransform(scrollYProgress, [0,1],['20vh','-20vh'], {ease: cB})
     const height = useTransform(scrollYProgress, [0,1],['100%','200%'], {ease: cB})
+    const scale = useTransform(scrollYProgress, [0,1],[1.5,1.5],{ease:cB});
     return (
-        <motion.div className="w-full h-[full] z-[10s] absolute pointer-events-none card overflow-hidden">
-            <motion.div className="halftone  w-full h-[400vh]" style={{y}}></motion.div>
+        <motion.div className="w-full h-screen z-[10s] absolute pointer-events-none card overflow-hidden">
+            <motion.div className="halftone  w-full h-[1000vh]" style={{y, scale}}></motion.div>
         </motion.div>
     );
 };
--- src/components/home/hyperPinkBreakpoints.tsx
+++ src/components/home/hyperPinkBreakpoints.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { useRef } from "react";
 import { motion, useScroll } from "framer-motion";
 import Logo from "../logo";
@@ -11,42 +11,26 @@ const HyperPinkBreakpoints = () => {
   const { scrollYProgress } = useScroll({ container: ref });
   const snapPanelClass = `w-screen h-full overflow-hidden snap-center`;
   return (
-    <motion.div
-      className="overflow-y-auto w-screen h-[80vh] lg:h-[90vh] snap-mandatory snap-y hyperPink"
-      layout
-      ref={ref}
-    >
+    <>
       <Logo width={200} scrollYProgress={scrollYProgress} />
       <TheDrip scrollYProgress={scrollYProgress} />
       <HalfToneLayer scrollYProgress={scrollYProgress} />
-      {/* These are individual divs so we can set snap-center on them */}
       <motion.div
-        viewport={{ amount: 0.5 }}
-        className={snapPanelClass}
-      />
-      <motion.div
-        viewport={{ amount: 0.5 }}
-        className={snapPanelClass}
-      />
-      <motion.div
-        viewport={{ amount: 0.5 }}
-        className={snapPanelClass}
-      />
-      <motion.div
-        viewport={{ amount: 0.5 }}
-        className={snapPanelClass}
-      />
-      <motion.div
-        viewport={{ amount: 0.5 }}
-        className={snapPanelClass}
-      />
-      <motion.div
-        viewport={{ amount: 0.5 }}
-        className={snapPanelClass}
+        className="overflow-y-auto w-screen h-[80vh] lg:h-[90vh] snap-mandatory snap-y hyperPink"
+        layout
+        ref={ref}
       >
-        <BlobbyHill scrollYProgress={scrollYProgress} />
+        {/* These are individual divs so we can set snap-center on them */}
+        <motion.div viewport={{ amount: 0.5 }} className={snapPanelClass} />
+        <motion.div viewport={{ amount: 0.5 }} className={snapPanelClass} />
+        <motion.div viewport={{ amount: 0.5 }} className={snapPanelClass} />
+        <motion.div viewport={{ amount: 0.5 }} className={snapPanelClass} />
+        <motion.div viewport={{ amount: 0.5 }} className={snapPanelClass} />
+        <motion.div viewport={{ amount: 0.5 }} className={snapPanelClass}>
+          <BlobbyHill scrollYProgress={scrollYProgress} />
+        </motion.div>
       </motion.div>
-    </motion.div>
+    </>
   );
 };
 
